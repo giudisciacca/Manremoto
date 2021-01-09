22fb33edab1d92939f1ea1235468fdadd40b926e
Giuseppe Di Sciacca <giuseppe.sciacca.17@ucl.ac.uk>
2021-01-09 14:51:34 +0100

    trial to get camera images

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 18 11:52:42 2019
-
 @author: gdisciac
 """
-## U-net for priors from optical ad us data
+## Manremoto
 
-import numpy
+import numpy as np
+import cv2
+
+def define_state():
+    state = 0;
+
+    return state
 
 def main():
-    print('hello world')
+    # connect to camera
+    cap = cv2.VideoCapture(0)
+
+    while (True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Display the resulting frame
+        cv2.imshow('frame', gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
 
 
 
