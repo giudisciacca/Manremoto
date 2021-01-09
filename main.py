1b101e902488a85f6b6bc111aaf2e5d45d9802a6
Giuseppe Di Sciacca <giuseppe.sciacca.17@ucl.ac.uk>
2021-01-09 14:17:15 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 11:52:42 2019
+
+@author: gdisciac
+"""
+## U-net for priors from optical ad us data
+
+import numpy
+
+def main():
+    print('hello world')
+
+
+
+main()
+
+
